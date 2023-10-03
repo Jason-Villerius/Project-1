54c969d7578ac40c5de03e1de22abb2de5ae727d
Jason-Villerius <9021149@student.zadkine.nl>
2023-10-03 09:46:10 +0200

    js

--- p1-javascript/opdracht_6.1/code.js
+++ p1-javascript/opdracht_6.1/code.js
@@ -2,3 +2,7 @@ function kleur_aanpassen (kleur)
 {
     document.getElementById("kop1").style.color = kleur;
 }
+function text_aanpassen ()
+{
+    document.getElementById("kop1").innerHTML = "Inhoud aangepast!";
+}
