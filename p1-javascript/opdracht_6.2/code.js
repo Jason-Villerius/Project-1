120c82216e69b9b8ab2415f45fd52656276a8957
Jason-Villerius <9021149@student.zadkine.nl>
2023-10-04 09:04:01 +0200

    js 6.2

---
+++ p1-javascript/opdracht_6.2/code.js
@@ -0,0 +1,13 @@
+function kleur_aanpassen (kleur)
+{
+    document.getElementById("kop1").style.color = kleur;
+}
+function text_aanpassen ()
+{
+    document.getElementById("kop1").innerHTML = "SOD1C";
+}
+function reset ()
+{
+    document.getElementById("kop1").style.color = "";
+    document.getElementById("kop1").innerHTML = "SYD";
+}
